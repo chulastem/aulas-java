ab72b2b96c64be9b9c6bb36e8c8f16e26f96640e
Pedro Antonio <68834162+chulastem@users.noreply.github.com>
2022-11-10 20:09:30 -0300

    Update Metodo Boolean.java

--- Metodo Boolean.java
+++ Metodo Boolean.java
@@ -1,3 +1,6 @@
+// Programa que recebe um valor e retorna True se o valor for par e False se o valor for impar
+// Utilizando o metodo boolean
+
 package javaapplication14;
 import java.util.Scanner;
 
@@ -23,7 +26,10 @@ public class JavaApplication14 {
     }
     
 }
-//Outro modo de fazer o metodo
+
+//Outro modo de fazer o metodo de uma forma mais simples
 
  public static boolean parOuImpar(int n){
        return n%2==0;
+
+//Por conta do metodo ser boolean, se o valor for 0 é true e ser for 1 é false
