89c94c1672745aae0d11d2b6550f30ce6fa6749e
Pedro Antonio <68834162+chulastem@users.noreply.github.com>
2022-11-22 15:35:09 -0300

    Add files via upload

---
+++ Exercicios - Estrutura sequencial/Main (10).java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);{
+        double valor;
+        System.out.println("Didite o valor gasto pelo cliente:");
+        valor = input.nextDouble();
+        System.out.println("Valor da gorjeta = R$ "+valor*0.10);
+    }
+    }
+ }
